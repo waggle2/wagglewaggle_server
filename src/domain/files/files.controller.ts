135c8eacf3e67f365dca998f936fbf01138e8cfd
donghyun123 <numnum97@naver.com>
2024-01-27 09:01:21 +0900

    Feat: 여러 개의 파일 삭제하는 API 추가

--- src/domain/files/files.controller.ts
+++ src/domain/files/files.controller.ts
@@ -10,11 +10,18 @@ import { ApiTags } from '@nestjs/swagger';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { UploadFilesDto } from '@/domain/files/dto/upload-files.dto';
 import { FilesService } from '@/domain/files/files.service';
+import { createS3Client } from '@/config/s3.config';
+import { ConfigService } from '@nestjs/config';
+import { DeleteObjectsCommand } from '@aws-sdk/client-s3';
+import { DeleteFilesDto } from '@/domain/files/dto/delete-files.dto';
 
 @ApiTags('files')
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @UseInterceptors(FilesInterceptor('files'))
   @Post()
@@ -25,5 +32,21 @@ export class FilesController {
     await this.filesService.uploadImagesToPost(files, uploadFilesDto);
 
     return { message: '이미지가 성공적으로 업로드되었습니다' };
+  }
+
+  @Delete()
+  async deleteFile(@Body() deleteFilesDto: DeleteFilesDto) {
+    const s3Client = createS3Client(this.configService);
+    const { keys } = deleteFilesDto;
+    const command = new DeleteObjectsCommand({
+      Bucket: this.configService.get('AWS_S3_BUCKET'),
+      Delete: {
+        Objects: keys.map((key) => {
+          return { Key: key };
+        }),
+      },
+    });
+
+    return await s3Client.send(command);
   }
 }
