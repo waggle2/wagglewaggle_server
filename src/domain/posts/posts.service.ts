06d1c9312e58e1486a5d51057f9963b799f5f46c
donghyun123 <numnum97@naver.com>
2024-01-29 06:02:30 +0900

    Fix: fix lint

--- src/domain/posts/posts.service.ts
+++ src/domain/posts/posts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException, Query } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
 import { Repository } from 'typeorm';
