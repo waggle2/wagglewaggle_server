fa9e133b62ce4d818bb437f8fec4fa5866da1172
eunli <eunli99012@gmail.com>
2024-02-01 06:49:26 +0900

    Fix: 오타 수정

--- src/domain/authentication/guards/adult-authentication.guard.ts
+++ src/domain/authentication/guards/adult-authentication.guard.ts
@@ -2,4 +2,4 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class AdultAuthenticationGuard extends AuthGuard('jwt') {}
+export class AdultAuthenticationGuard extends AuthGuard('adult') {}
