dcae97fb365e09b3649fd2f2db596d41df873d8b
eunli <eunli99012@gmail.com>
2024-02-01 05:42:42 +0900

    Feat: 성인인증 guard, strategy 구현 #2

--- src/domain/authentication/passport/jwt.strategy.ts
+++ src/domain/authentication/passport/jwt.strategy.ts
@@ -12,7 +12,7 @@ import TokenPayload from '../interfaces/token-payload.interface';
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     private readonly configService: ConfigService,
-    private readonly userService: UsersService,
+    private readonly usersService: UsersService,
     private readonly authenticationService: AuthenticationService,
   ) {
     super({
@@ -36,14 +36,14 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       );
     }
     // 액세스 토큰 검증
-    const foundUser = await this.userService.findById(payload.userId);
+    const foundUser = await this.usersService.findById(payload.userId);
     if (!foundUser) {
       // 액세스 토큰이 만료된 경우 리프레시 토큰 검증(유효하다면 액세스 토큰 재발급)
       const refreshToken = req.cookies.refreshToken as string;
       if (!refreshToken) {
         throw new UnauthorizedException('인증되지 않은 사용자입니다.');
       }
-      const refreshUser = await this.userService.findById(payload.userId);
+      const refreshUser = await this.usersService.findById(payload.userId);
       if (!refreshUser) {
         throw new UnauthorizedException('새로 로그인해야 합니다.');
       }
