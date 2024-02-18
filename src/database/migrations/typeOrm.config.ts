bbd17fcb551b57ad22078300c5e29fc536f0aba0
donghyun123 <numnum97@naver.com>
2024-02-18 18:08:55 +0900

    Feat: Animal enum 변경 및 마이그레이션

---
+++ src/database/migrations/typeOrm.config.ts
@@ -0,0 +1,19 @@
+import { config } from 'dotenv';
+import { ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+import { AnimalEnumRefactoring1708161878890 } from '@/database/migrations/1708161878890-AnimalEnumRefactoring';
+
+config();
+
+const configService = new ConfigService();
+
+export default new DataSource({
+  type: 'mysql',
+  host: configService.get(`DB_HOST`),
+  port: configService.get(`DB_PORT`),
+  username: configService.get(`DB_USERNAME`),
+  password: configService.get(`DB_PASSWORD`),
+  database: configService.get(`DB_NAME`),
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  migrations: [AnimalEnumRefactoring1708161878890],
+});
