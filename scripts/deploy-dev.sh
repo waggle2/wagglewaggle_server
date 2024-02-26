9bd4ca2fafd952b6adb8f1a252161ac3d3cb26c7
donghyun123 <numnum97@naver.com>
2024-02-26 17:41:19 +0900

    Chore: 로컬에서 이전 이미지들 삭제하는 로직 추가

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -2,6 +2,7 @@
 
 REPOSITORY=~/wagglewaggle_server
 DOCKER_COMPOSE_FILE=${REPOSITORY}/docker-compose.dev.yml
+IMAGE_TAG=$(date '+%Y%m%d%H%M%S')
 
 echo "> 프로젝트 폴더로 이동"
 cd $REPOSITORY || exit 1
@@ -9,8 +10,10 @@ cd $REPOSITORY || exit 1
 echo "> git pull"
 git pull origin dev || exit 1
 
-echo "> 소스코드 빌드"
+echo "> 소스코드 빌드 및 태그 지정"
 docker-compose -f $DOCKER_COMPOSE_FILE build || exit 1
+docker tag wagglewaggle_server-app_5001:latest wagglewaggle_server-app_5001:"$IMAGE_TAG"
+docker tag wagglewaggle_server-app_5002:latest wagglewaggle_server-app_5002:"$IMAGE_TAG"
 
 echo "> 현재 실행중인 컨테이너 확인"
 
@@ -50,5 +53,8 @@ fi
 
 echo "> 5002번 포트에 새 컨테이너 실행"
 docker-compose -f $DOCKER_COMPOSE_FILE up -d app_5002 || exit 1
+
+echo "> 이전 이미지 삭제"
+docker images | grep wagglewaggle_server-app | grep -v "$IMAGE_TAG" | awk '{print $3}' | xargs -r docker rmi
 
 echo "> 배포 완료"
